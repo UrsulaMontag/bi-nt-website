73f9cbc6a0e5fe407af50d688dfcec119500ea16
U_Montag <montagu666@gmail.com>
2023-03-10 14:50:48 +0100

    adjust links

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,19 +4,23 @@ import { NavContainer, NavItem, NavList, HamburgerMenu } from "./navbar.styled";
 import { withRouter } from "next/router";
 
 const pages = {
-  "/": [],
-  "/news": [],
-  "/events": [],
-  "/pictures": [],
-  "/background-information": [
-    { path: "/officials", label: "Offizielles von Stadt und Presse" },
-    { path: "/history", label: "Geschichte" },
-    { path: "/team", label: "Team" },
-  ],
-  "/donations": [],
-  "/contact": [],
+  "/": { path: [], label: "Über Uns" },
+  "/news": { path: [], label: "Aktuelles" },
+  "/events": { path: [], label: "Veranstaltungen" },
+  "/pictures": { path: [], label: "Bilder" },
+  "/background-information": {
+    path: [
+      { path: "/officials", label: "Offizielles von Stadt und Presse" },
+      { path: "/in-demand", label: "Nachgefragt" },
+      { path: "/alternatives", label: "Unsere Alternativ-Vorschläge" },
+      { path: "/flora-fauna", label: "Flora und Fauna" },
+      { path: "/history", label: "Geschichte" },
+    ],
+    label: "Bilder",
+  },
+  "/donations": { path: [], label: "Spenden" },
+  "/contact": { path: [], label: "Kontakt" },
 };
-
 const Nav = () => {
   const [showNav, setShowNav] = useState(false);
   const [showSubNav, setSubShowNav] = useState(false);
@@ -31,42 +35,49 @@ const Nav = () => {
 
   const handleLinkClick = (path) => {
     setCurrentPath(path);
-    setShowNav(false);
-    setSubShowNav(false);
+    toggleSubNav();
+    toggleNav();
   };
 
   const generateNavLinks = (currentPath) => {
-    return Object.keys(pages).map((path) => {
-      const hasSubNav = pages[path].length > 0;
+    return (
+      pages &&
+      Object.keys(pages)?.map((path) => {
+        const page = pages[path];
+        const hasSubNav = page?.path && page?.path.length > 0;
 
-      return (
-        <NavItem key={path}>
-          <Link
-            href={path}
-            passHref
-            className={currentPath === path ? "active" : ""}
-            onClick={() => handleLinkClick(path)}
-          >
-            {path}
-          </Link>
-          {hasSubNav && (
-            <ul>
-              {pages[path].map((subPage) => (
-                <NavItem key={subPage.path}>
-                  <Link
-                    href={path + subPage.path}
-                    passHref
-                    onClick={() => handleLinkClick(path)}
-                  >
-                    {subPage.label}
-                  </Link>
-                </NavItem>
-              ))}
-            </ul>
-          )}
-        </NavItem>
-      );
-    });
+        return (
+          <NavItem key={path}>
+            <Link
+              href={path}
+              passHref
+              className={currentPath === path ? "active" : ""}
+              onClick={() => handleLinkClick(path)}
+            >
+              {page?.label || path}
+            </Link>
+            {hasSubNav && (
+              <ul>
+                {page.path.map((subPage) => (
+                  <NavItem key={subPage.path}>
+                    <Link
+                      href={path + subPage.path}
+                      passHref
+                      onClick={() => handleLinkClick(path + subPage.path)}
+                      className={
+                        currentPath === path + subPage.path ? "active" : ""
+                      }
+                    >
+                      {subPage.label}
+                    </Link>
+                  </NavItem>
+                ))}
+              </ul>
+            )}
+          </NavItem>
+        );
+      })
+    );
   };
 
   return (
